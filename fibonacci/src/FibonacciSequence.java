baf2f5a6d893c3decd51c9c0ecedc21f2db4037f
miritta <miri_terra@mail.ru>
2014-04-21 00:40:41 +0700

    Fibonacci sequence
    A Fibonacci sequence is the sequence of numbers 1, 1, 2, 3, 5, 8, 13,
    21, 34, and so on, where each number (from the third on) is the sum of
    the previous two. Create a method that takes an integer as an argument
    and displays that many Fibonacci numbers starting from the beginning,
    e.g., If you run java Fibonacci 5 (where Fibonacci is the name of the
    class) the output will be: 1, 1, 2, 3, 5.

---
+++ fibonacci/src/FibonacciSequence.java
@@ -0,0 +1,29 @@
+public class FibonacciSequence {
+    public void Fibonacci(int edge) {
+        StringBuilder builder = new StringBuilder("1");//create an object String for print
+        int prev = 0;//previous item
+        int cur = 1;//current item
+        int tmp = 0;//intermediate item
+        while (cur != edge) {
+            tmp = cur;
+            cur += prev;
+            prev = tmp;
+            if (cur < edge) {
+                builder.append(", ").append(cur);
+            }else break;
+        }
+        System.out.println(builder.toString());
+    }
+
+    public static void main(String[] arg) {
+        try {
+            if (Integer.valueOf(arg[0]) == 0) {
+                System.out.println("O isn't an item of this sequence");
+            } else {
+                new FibonacciSequence().Fibonacci(Integer.valueOf(arg[0]));
+            }
+        } catch (NumberFormatException | IndexOutOfBoundsException e) {
+            System.out.println("Incorrect data");
+        }
+    }
+}
