1e8aad0a413b75562c5dbe94dc11ff60621afb83
miritta <miri_terra@mail.ru>
2014-04-21 15:04:03 +0700

    fixed Fibonacci
    A Fibonacci sequence is the sequence of numbers 1, 1, 2, 3, 5, 8, 13,
    21, 34, and so on, where each number (from the third on) is the sum of
    the previous two. Create a method that takes an integer as an argument
    and displays that many Fibonacci numbers starting from the beginning,
    e.g., If you run java Fibonacci 5 (where Fibonacci is the name of the
    class) the output will be: 1, 1, 2, 3, 5.

--- fibonacci/src/FibonacciSequence.java
+++ fibonacci/src/FibonacciSequence.java
@@ -1,26 +1,28 @@
 public class FibonacciSequence {
-    private void Fibonacci(int edge) {
-        StringBuilder builder = new StringBuilder("1");//create an object String for print
+    private static void calculate(int edge) {
+        StringBuilder builder = new StringBuilder();//create an object String for print
         int prev = 0;//previous item
         int cur = 1;//current item
         int tmp = 0;//intermediate item
-        while (cur != edge) {
+        for (int i = 0; i < edge; i++) {
+            builder.append(cur);
             tmp = cur;
             cur += prev;
             prev = tmp;
-            if (cur < edge) {
-                builder.append(", ").append(cur);
-            }else break;
+            if (i<(edge-1)) {
+                builder.append(", ");
+            }
         }
         System.out.println(builder.toString());
     }
 
     public static void main(String[] arg) {
         try {
-            if (Integer.valueOf(arg[0]) == 0) {
-                System.out.println("O isn't an item of this sequence");
+            int number = Integer.valueOf(arg[0]);
+            if (number>=0) {
+                calculate(number);
             } else {
-                new FibonacciSequence().Fibonacci(Integer.valueOf(arg[0]));
+                System.out.println("You entered a negative number");
             }
         } catch (NumberFormatException | IndexOutOfBoundsException e) {
             System.out.println("Incorrect data");
