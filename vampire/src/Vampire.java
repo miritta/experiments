e5c53784e99834d649034f523b3ade3ac00fd25b
miritta <miri_terra@mail.ru>
2014-04-23 21:31:52 +0700

    methods and fields are added
    A vampire number has an even number of digits and is formed by
    multiplying a pair of numbers containing half the number of digits of
    the result. The digits are taken from the original number in any order.
    Pairs of trailing zeroes are not allowed. Examples include:
    1260 = 21 * 60
    1827 = 21 * 87
    2187 = 27 * 81
    Write a program that finds all the 4-digit vampire numbers.

--- vampire/src/Vampire.java
+++ vampire/src/Vampire.java
@@ -1,43 +1,87 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Vampire {
-    private List<Integer> vampireCalculate(int size) {
+    List<String> multipliers;
+    List<Integer> results;
+
+    Vampire() {
+        multipliers = new ArrayList<String>();
+        results = new ArrayList<Integer>();
+    }
+
+    private List<Integer> intToList(int base, List<Integer> list) {
+        String s = Integer.toString(base);
+        for (int i = 0; i < s.length(); i++) {
+            list.add(Character.getNumericValue(s.charAt(i)));
+        }
+        return list;
+    }
+
+    private boolean compareLists(List<Integer> list1, List<Integer> list2) {
+        Collections.sort(list1);
+        Collections.sort(list2);
+        return list1.equals(list2);
+    }
+
+    private void calculate(int size) {
         List<Integer> multi = new ArrayList<Integer>();
         List<Integer> parts = new ArrayList<Integer>();
-        List<Integer> results = new ArrayList<Integer>();
-        int max = (int) Math.pow(10, size / 2);
         int min = (int) Math.pow(10, size / 2 - 1);
+        int max = min * 10;
         for (int num1 = min; num1 < max; num1++) {
             for (int num2 = min; num2 <= num1; num2++) {
-                if ((num1 * num2 % 9) != (num1 + num2) % 9) {// Pete Hartley's theoretical result: If x·y is a vampire number then x·y == x+y (mod 9)
+                if ((num1 * num2 % 9) != (num1 + num2) % 9) {// If x·y is a vampire number then x·y == x+y (mod 9)
                     continue;
                 }
-                int sum = num1 * num2;
-                for (int j = 0; j < size / 2; j++) {
-                    parts.add((int) ((num1 % Math.pow(10, j + 1)) / Math.pow(10, j)));
-                    parts.add((int) ((num2 % Math.pow(10, j + 1)) / Math.pow(10, j)));
-                }
-                String s = Integer.toString(sum);
-                for (int i = 0; i < s.length(); i++) {
-                    multi.add(Character.getNumericValue(s.charAt(i)));
-                }
-                Collections.sort(parts);
-                Collections.sort(multi);
-                if (parts.equals(multi)) {
-                    results.add(sum);
+                int product = num1 * num2;
+                intToList(num1, parts);
+                intToList(num2, parts);
+                intToList(product, multi);
+                if (compareLists(parts, multi)) {
+                    results.add(product);
+                    multipliers.add(num1 + "*" + num2);
                 }
                 parts.clear();
                 multi.clear();
             }
         }
-        return results;
+    }
+
+    private void printList(List list) {
+        for (Object a : list) {
+            System.out.println(a);
+        }
+    }
+
+    private void printResults(List list1, List list2) {
+        for (int i = 0; i < list1.size(); i++) {
+            System.out.println(list1.get(i) + "=" + list2.get(i));
+        }
+    }
+
+    private void performAll(int dim) {
+        calculate(dim);
+        printList(results);
+        printResults(multipliers, results);
     }
 
     public static void main(String[] arg) {
-        int dim = 4;
-        System.out.println(Arrays.toString(new Vampire().vampireCalculate(dim).toArray()));
+        Vampire vamp = new Vampire();
+        if (arg.length != 0) {
+            try {
+                int dim = Integer.valueOf(arg[0]);
+                if ((dim >= 0) & (dim % 2 == 0)) {
+                    vamp.performAll(dim);
+                } else {
+                    System.out.println("You entered a negative or odd value");
+                }
+            } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                System.out.println("Incorrect data");
+            }
+        } else {
+            vamp.performAll(4);
+        }
     }
 }
